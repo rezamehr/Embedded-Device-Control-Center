68c86c3d87c95ec8d9e2f3c0399243ed0378df0f
Reza Mehrabani <39555446+rezamehr@users.noreply.github.com>
2026-08-05 11:01:03 +0330

    SerialCommunication header ver1.0

--- src/communication/SerialCommunication.h
+++ src/communication/SerialCommunication.h
@@ -1,1 +1,38 @@
 
+#pragma once
+
+#include "core/ICommunication.h"
+#include <QSerialPort>
+
+namespace edcc {
+
+class SerialCommunication : public ICommunication
+{
+    Q_OBJECT
+
+public:
+    explicit SerialCommunication(const QString &portName,
+                                 qint32 baudRate = 115200,
+                                 QObject *parent = nullptr);
+    ~SerialCommunication() override;
+
+    bool open() override;
+    void close() override;
+    bool isOpen() const override;
+    qint64 send(const QByteArray &data) override;
+    QString name() const override;
+
+    void setBaudRate(qint32 baudRate);
+    qint32 baudRate() const;
+
+private slots:
+    void onReadyRead();
+    void onErrorOccurred(QSerialPort::SerialPortError error);
+
+private:
+    QSerialPort *m_serial;
+    QString m_portName;
+    qint32 m_baudRate;
+};
+
+} // namespace edcc
