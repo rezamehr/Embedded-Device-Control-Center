dbd02c54261f6ecc4ff682d82797a5ed9f6c39a5
Reza Mehrabani <39555446+rezamehr@users.noreply.github.com>
2026-08-05 10:54:56 +0330

    Main ver 1.0

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,14 +1,11 @@
-
 #include <QApplication>
-#include <QMainWindow>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QMainWindow window;
-    window.setWindowTitle("Embedded Device Control Center");
-    window.resize(1100, 700);
+    edcc::MainWindow window;
     window.show();
 
     return app.exec();
