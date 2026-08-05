be9d731667c4f208e182d4ea84d50c1c415e07ee
Reza Mehrabani <39555446+rezamehr@users.noreply.github.com>
2026-08-05 16:20:08 +0330

    Add new Info device

--- src/core/Types.h
+++ src/core/Types.h
@@ -27,6 +27,8 @@ struct DeviceInfo {
     QString portName;      // for serial
     QString host;          // for TCP
     quint16 port = 0;      // for TCP
+    qint32  baudRate = 115200;   // for serial
+    QString type;          // "serial" or "tcp"
 };
 
 } // namespace edcc
