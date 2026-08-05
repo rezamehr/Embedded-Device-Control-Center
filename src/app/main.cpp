a9ca28975c73636a80f6023c7e46595b28ca3c8c
Reza Mehrabani <39555446+rezamehr@users.noreply.github.com>
2026-08-05 09:47:41 +0330

    Update main.cpp

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,1 +1,15 @@
 
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QMainWindow window;
+    window.setWindowTitle("Embedded Device Control Center");
+    window.resize(1100, 700);
+    window.show();
+
+    return app.exec();
+}
