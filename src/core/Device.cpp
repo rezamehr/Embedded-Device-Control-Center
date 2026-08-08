020badeab916fbb025812886b21d3d521560eba5
Reza Mehrabani <39555446+rezamehr@users.noreply.github.com>
2026-08-08 10:18:59 +0330

    add CommunicationWorker
    use moveToThread start

--- src/core/Device.cpp
+++ src/core/Device.cpp
@@ -1,4 +1,6 @@
 #include "Device.h"
+#include "communication/CommunicationWorker.h"
+#include "logging/Logger.h"
 
 namespace edcc {
 
@@ -7,24 +9,50 @@ Device::Device(const DeviceInfo &info,
                QObject *parent)
     : IDevice(parent)
     , m_info(info)
-    , m_comm(communication)
 {
-    if (m_comm) {
-        // Take ownership
-        m_comm->setParent(this);
-
-        connect(m_comm, &ICommunication::stateChanged,
-                this, &Device::onCommStateChanged);
-        connect(m_comm, &ICommunication::dataReceived,
-                this, &Device::onCommDataReceived);
-        connect(m_comm, &ICommunication::errorOccurred,
-                this, &Device::onCommError);
-    }
+    setupWorker(communication);
 }
 
 Device::~Device()
 {
-    disconnectFromDevice();
+    // Clean shutdown of the worker thread
+    if (m_thread) {
+        if (m_worker) {
+            // Request close from the worker thread
+            QMetaObject::invokeMethod(m_worker, "close", Qt::QueuedConnection);
+        }
+
+        m_thread->quit();
+        if (!m_thread->wait(3000)) {
+            Logger::instance().warning("Worker thread did not finish in time, terminating", m_info.id);
+            m_thread->terminate();
+            m_thread->wait(1000);
+        }
+    }
+}
+
+void Device::setupWorker(ICommunication *communication)
+{
+    m_thread = new QThread(this);
+    m_worker = new CommunicationWorker(communication); // no parent – we move it
+
+    // Move worker to its dedicated thread
+    m_worker->moveToThread(m_thread);
+
+    // Clean up worker when thread finishes
+    connect(m_thread, &QThread::finished, m_worker, &QObject::deleteLater);
+
+    // Forward signals from worker to Device (and then to outside)
+    connect(m_worker, &CommunicationWorker::stateChanged,
+            this, &Device::onWorkerStateChanged);
+    connect(m_worker, &CommunicationWorker::dataReceived,
+            this, &Device::onWorkerDataReceived);
+    connect(m_worker, &CommunicationWorker::errorOccurred,
+            this, &Device::onWorkerError);
+    connect(m_worker, &CommunicationWorker::bytesWritten,
+            this, &Device::onWorkerBytesWritten);
+
+    m_thread->start();
 }
 
 QString Device::id() const
@@ -44,17 +72,20 @@ QString Device::description() const
 
 bool Device::connectToDevice()
 {
-    if (!m_comm)
+    if (!m_worker)
         return false;
 
-    return m_comm->open();
+    // Call open() in the worker thread
+    QMetaObject::invokeMethod(m_worker, "open", Qt::QueuedConnection);
+    return true; // actual result comes asynchronously via stateChanged
 }
 
 void Device::disconnectFromDevice()
 {
-    if (m_comm) {
-        m_comm->close();
-    }
+    if (!m_worker)
+        return;
+
+    QMetaObject::invokeMethod(m_worker, "close", Qt::QueuedConnection);
 }
 
 bool Device::isConnected() const
@@ -69,10 +100,14 @@ ConnectionState Device::state() const
 
 qint64 Device::send(const QByteArray &data)
 {
-    if (!m_comm || !isConnected())
+    if (!m_worker || m_state != ConnectionState::Connected)
         return -1;
 
-    return m_comm->send(data);
+    // Fire-and-forget – actual write happens in worker thread
+    QMetaObject::invokeMethod(m_worker, "send",
+                              Qt::QueuedConnection,
+                              Q_ARG(QByteArray, data));
+    return data.size(); // optimistic
 }
 
 DeviceInfo Device::info() const
@@ -80,20 +115,26 @@ DeviceInfo Device::info() const
     return m_info;
 }
 
-void Device::onCommStateChanged(ConnectionState state)
+void Device::onWorkerStateChanged(ConnectionState state)
 {
     m_state = state;
     emit stateChanged(state);
 }
 
-void Device::onCommDataReceived(const QByteArray &data)
+void Device::onWorkerDataReceived(const QByteArray &data)
 {
     emit dataReceived(data);
 }
 
-void Device::onCommError(const QString &error)
+void Device::onWorkerError(const QString &error)
 {
     emit errorOccurred(error);
+}
+
+void Device::onWorkerBytesWritten(qint64 bytes)
+{
+    Q_UNUSED(bytes);
+    // Can be used later for accurate TX statistics
 }
 
 } // namespace edcc
